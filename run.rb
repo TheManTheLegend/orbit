8c6c15ff4db93ed1f50f9220f618a9dd7264ee99
Nik Young <youngnr@s.dcsdk12.org>
2015-11-18 11:05:33 -0700

    i did things

---
+++ run.rb
@@ -0,0 +1,55 @@
+require "gosu"
+require_relative "satelite"
+
+class GameWindow < Gosu::Window
+	def initialize
+		super 480, 480
+		@planets = []
+
+		File.open("planets.txt", "r", '\r').each_with_index do |line, index| 
+			if index == 0 
+				bodies = line.to_i
+			elsif index == 1
+				universe = line.to_i
+			else
+	      x, y, x_vel, y_vel, mass, image = line.strip.split(' ') 
+	      @planets = Satelite.new(:x => x, :y => y, :x_vel => x_vel, :y_vel => y_vel, :mass => mass, :image => image, :universe => universe)
+	      @planets.save
+	    end
+	  end
+		# file = File.open("planets.txt", 'r')
+		# file.each_line_with_index do |line, index|
+		# 	if index == 0 
+		# 		@bodies = line.to_i
+		# 	elsif index == 1
+		# 		@universe = line.to_i
+		# 	else
+		# 		array = line.split(" ")
+		# 		@planets.push(Satelite.new(array[0].to_i, array[1].to_i, array[2].to_i, array[3].to_i, array[4].to_i, array[5].to_s, @universe))
+		# 	end
+		# end
+
+		@background_image = Gosu::Image.new("images/starmap.jpg", :tileable => true)
+
+	end
+
+	def update
+		(0..@planets.length).each do |index|
+			@planets[index].move
+		end
+	end
+
+	def draw
+		(0..@planets.length).each do |index|
+			@planets[index].draw
+		end
+		
+		@background_image.draw(0, 0, 1)
+	end
+
+
+
+end
+
+window = GameWindow.new
+window.show
---
+++ satelite.rb
@@ -0,0 +1,31 @@
+require "gosu"
+
+class Satelite
+	def initialize(x_pos, y_pos, x_vel, y_vel, mass, image, universe)
+		@univers = universe
+		@x = (x_pos/@universe)*width + width/2
+		@y = (y_pos/@universe)*height + height/2
+		@x_vel = (x_vel/@universe)*width + width/2
+		@y_vel = (y_vel/@universe)*height + height/2
+		@image = Gosu::Image.new("images/#{image}.png")
+		@mass = mass
+		@angle = tan(@y/@x)
+	end
+
+	def move
+		@angle = tan(@y/@x)
+		@x_vel = @x_vel*(cos(@angle))
+		@y_vel = @y_vel*(sin(@angle))
+
+
+
+		@x = @x + @x_vel
+		@y = @y + @y_vel
+	end
+
+	def draw
+		@image.draw_rot(@x, @y, 2, 0)
+	end
+
+end
+
